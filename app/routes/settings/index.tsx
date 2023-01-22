81448092c1efe8341f330c2f31b17c8c796697ab
Abdullah Bl <abdullah-dev@hotmail.com>
2023-01-23 01:20:33 +0300

    simple export

--- app/routes/settings/index.tsx
+++ app/routes/settings/index.tsx
@@ -1,8 +1,9 @@
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+import { ExclamationTriangleIcon, Share2Icon } from '@radix-ui/react-icons'
 import type { ActionFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 import { db } from '~/db.server'
+import * as Sheets from 'xlsx'
 
 export const loader: ActionFunction = async () => {
 	const data = db.get()
@@ -28,6 +29,27 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function Index() {
 	const data = useLoaderData<typeof loader>()
+	const download = () => {
+		const wb = Sheets.utils.book_new()
+		const budget = Sheets.utils.json_to_sheet(data.budget)
+		const projects = Sheets.utils.json_to_sheet(data.projects)
+		const phases = Sheets.utils.json_to_sheet(data.phases)
+		Sheets.utils.book_append_sheet(wb, budget, 'Budget')
+		Sheets.utils.book_append_sheet(wb, projects, 'Projects')
+		Sheets.utils.book_append_sheet(wb, phases, 'Phases')
+		Sheets.writeFile(wb, 'gen.xlsx')
+	}
+
+	const exportAsJSON = () => {
+		const json = JSON.stringify(data)
+		const blob = new Blob([json], { type: 'application/json' })
+		const url = URL.createObjectURL(blob)
+		const a = document.createElement('a')
+		a.download = 'gen.json'
+		a.href = url
+		a.click()
+	}
+
 	return (
 		<div className='flex-1'>
 			<h1 className='font-bold'>General</h1>
@@ -41,6 +63,22 @@ export default function Index() {
 					<ExclamationTriangleIcon />
 				</button>
 			</Form>
+			<button
+				type='button'
+				aria-label='Export'
+				title='Export Data as XLSX'
+				onClick={download}
+			>
+				<Share2Icon width={22} height={22} />
+			</button>
+			<button
+				type='button'
+				aria-label='Export'
+				title='Export Data as JSON'
+				onClick={exportAsJSON}
+			>
+				<Share2Icon width={22} height={22} />
+			</button>
 			<pre className=' overflow-y-scroll h-auto'>
 				{JSON.stringify(data, null, 2)}
 			</pre>
--- app/routes/settings/projects.tsx
+++ app/routes/settings/projects.tsx
@@ -3,19 +3,27 @@ import { json } from '@remix-run/node'
 import { defer } from '@remix-run/node'
 import { Await, useLoaderData } from '@remix-run/react'
 import { Suspense } from 'react'
+import * as Sheets from 'xlsx'
 import { db } from '~/db.server'
 
 export const loader: LoaderFunction = async () => {
-	return json({
+	return defer({
 		projects: db.get().projects,
 	})
 }
 
 export default function Index() {
 	const data = useLoaderData<typeof loader>()
+	const download = () => {
+		const wb = Sheets.utils.book_new()
+		const ws = Sheets.utils.json_to_sheet(data.projects)
+		Sheets.utils.book_append_sheet(wb, ws, 'Projects')
+		Sheets.writeFile(wb, 'projects.xlsx')
+	}
 	return (
 		<>
 			<h1 className='font-bold'>Projects</h1>
+			<button onClick={download}>Download as xlsx</button>
 			<Suspense fallback={<div>Loading...</div>}>
 				<Await resolve={data} errorElement={<h2> Error </h2>}>
 					{(data) => (
